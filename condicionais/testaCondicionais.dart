988a54254b2098c5c83a497b45b88e40b42714c3
Robertdesenvolvermec <robertsanttos.rs@gmail.com>
2021-02-18 16:33:13 -0300

    Condicionais em Dart

---
+++ condicionais/testaCondicionais.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  print('testando as condicionais.');
+
+  int idade = 21;
+
+  if (idade >= 21) {
+    print('Você pode beber!');
+  } else {
+    print('Você não pode beber');
+  }
+}
