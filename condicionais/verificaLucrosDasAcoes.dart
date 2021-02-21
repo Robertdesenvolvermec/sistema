c9aa2b530bfd245594b798f5c8ec1eac601838ae
Robert dos Santos Silva <60759036+Robertdesenvolvermec@users.noreply.github.com>
2021-02-21 20:57:23 -0300

    Sistema de ações
    Sistema que verifica o lucro ou prejuízo na venda de ações

--- condicionais/verificaLucrosDasAcoes.dart
+++ condicionais/verificaLucrosDasAcoes.dart
@@ -1,9 +1,23 @@
 void main() {
-  double diaDeCompra = 3.45;
-  double diaDeVenda = 4.23;
-  int quantidade = 50;
+  double diaDeCompra = 5.00;
+  double diaDeVenda = 6.00;
+  int quantidade = 1;
 
-  if(quantidade > diaDeCompra){
+  double resultadoCompra = quantidade * diaDeCompra;
+  double resultadoVenda = quantidade * diaDeVenda;
+  double verificaResultado = resultadoVenda - resultadoCompra;
+
+  if(resultadoCompra > resultadoVenda){
     
+    print('Valor da compra: $resultadoCompra');
+    print('Valor de venda: $resultadoVenda');
+    print('prejuizo');
+    print(verificaResultado);
+  } else {
+
+    print('Valor da compra: $resultadoCompra');
+    print('Valor de venda: $resultadoVenda');
+    print('lucro');
+    print(verificaResultado);
   }
 }
